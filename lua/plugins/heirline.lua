dc19b77dffe86e4bd936a25e74e6bb416b2f9257
Daniel Perez Alvarez <daniel@unindented.org>
2024-08-17 15:53:55 -0700

    Remove clock from status line

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -18,31 +18,31 @@ return {
     }
 
     -- add date/time to last indicator
-    opts.statusline[#opts.statusline] = status.component.builder {
-      {
-        provider = function()
-          local datetime = os.date "%Y-%m-%d %H:%M"
-          ---@cast datetime string
-          return status.utils.stylize(datetime, {
-            icon = { kind = "Clock", padding = { right = 1 } },
-            padding = { left = 1, right = 1 },
-          })
-        end,
-      },
-      update = { "ModeChanged", pattern = "*:*", callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end) },
-      hl = status.hl.get_attributes "mode",
-      surround = { separator = "right", color = status.hl.mode_bg },
-      init = status.init.update_events {
-        { "User", pattern = "UpdateTime", callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end) },
-      },
-    }
+    --opts.statusline[#opts.statusline] = status.component.builder {
+    --  {
+    --    provider = function()
+    --      local datetime = os.date "%Y-%m-%d %H:%M"
+    --      ---@cast datetime string
+    --      return status.utils.stylize(datetime, {
+    --        icon = { kind = "Clock", padding = { right = 1 } },
+    --        padding = { left = 1, right = 1 },
+    --      })
+    --    end,
+    --  },
+    --  update = { "ModeChanged", pattern = "*:*", callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end) },
+    --  hl = status.hl.get_attributes "mode",
+    --  surround = { separator = "right", color = status.hl.mode_bg },
+    --  init = status.init.update_events {
+    --    { "User", pattern = "UpdateTime", callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end) },
+    --  },
+    --}
 
     -- timer for updating the time
-    local uv = vim.uv or vim.loop
-    uv.new_timer():start(
-      (60 - tonumber(os.date "%S")) * 1000, -- offset timer based on current seconds past the minute
-      60 * 1000, -- update every 60 seconds
-      vim.schedule_wrap(function() vim.api.nvim_exec_autocmds("User", { pattern = "UpdateTime", modeline = false }) end)
-    )
+    --local uv = vim.uv or vim.loop
+    --uv.new_timer():start(
+    --  (60 - tonumber(os.date "%S")) * 1000, -- offset timer based on current seconds past the minute
+    --  60 * 1000, -- update every 60 seconds
+    --  vim.schedule_wrap(function() vim.api.nvim_exec_autocmds("User", { pattern = "UpdateTime", modeline = false }) end)
+    --)
   end,
 }
