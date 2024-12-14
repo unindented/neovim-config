9c8155d4b3bad46cf2c4c3bd244fd358d2c10118
Daniel Perez Alvarez <daniel@unindented.org>
2024-12-13 21:36:00 -0800

    Remove conflicting mapping

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -7,14 +7,12 @@ return {
       n = {
         [":"] = { ";", noremap = true },
         [";"] = { ":", noremap = true },
-        ["<tab>"] = { "za", noremap = true },
-        ["<leader><leader>"] = { "<c-^>", noremap = true },
-        ["<leader>d"] = { '"_d', noremap = true, silent = true },
+        ["<Tab>"] = { "za", noremap = true },
+        ["<Leader><Leader>"] = { "<C-^>", noremap = true },
       },
       v = {
         [":"] = { ";", noremap = true },
         [";"] = { ":", noremap = true },
-        ["<leader>d"] = { '"_d', noremap = true, silent = true },
       },
     },
   },
